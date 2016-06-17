1f8c3f6a23491f0bb79c9678e3cf31e02813449b
irenliber <irr.morgo@gmail.com>
2016-06-17 15:13:07 +0600

    added resources :pages, only: [:show]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/show'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,5 +8,7 @@ Rails.application.routes.draw do
   root 'notes#index'
 
   resources :notes
+
+  resources :pages, only: [:show]
 
 end
